dc79d8649b4535aafddbf984f76979989ad833db
Aman Kumar <aman.kumar.connect@gmail.com>
2026-02-09 14:23:43 +0530

    add shadcn

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button"
+
 function App(): React.JSX.Element {
   return (
     <>
       <h1>Start with electron-vite, redux toolkit, tailwind</h1>
+      <Button>This is a shadcn button</Button>
     </>
   )
 }
